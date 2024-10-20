a4cd4825aff19f31c51b830e137e7595ca5f7f7a
absent-friend <forsorenmueller@gmail.com>
2024-10-20 17:43:51 -0400

    tweak light mode heading text color; didn't actually need conditional logic for the base URL

--- layouts/LayoutDefault.tsx
+++ layouts/LayoutDefault.tsx
@@ -8,7 +8,7 @@ export default function LayoutDefault(props: { children?: JSX.Element }) {
       <header class="border-b border-zinc-400 grid p-4">
         <Link href="/" noUnderline={true}>
           <img src={import.meta.env.BASE_URL + 'favicon.png'} class="inline size-12 lg:size-auto" />
-          <span class="align-middle font-bold italic ms-1 text-4xl lg:text-6xl text-violet-900 dark:text-violet-300">Kill the Sorceress!</span>
+          <span class="align-middle font-bold italic ms-1 text-4xl lg:text-6xl text-indigo-900 dark:text-violet-300">Kill the Sorceress!</span>
         </Link>
       </header>
       <div class="overflow-auto" style="scrollbar-width: thin;">{props.children}</div>
