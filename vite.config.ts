81a8aca7edcc24c7a1a1aa5f04b0964d99084ee1
absent-friend <forsorenmueller@gmail.com>
2024-10-20 16:19:41 -0400

    Use different base URL in production

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import vikeSolid from "vike-solid/vite";
 import { defineConfig } from "vite";
 import vike from "vike/plugin";
 
-export default defineConfig({
-  // base: '/kill-the-sorceress',
+export default defineConfig(({ command, mode, isSsrBuild, isPreview }) => ({
+  base: isPreview ? '/' : '/kill-the-sorceress',
   plugins: [vike({ prerender: true }), vikeSolid()],
-});
+}));
