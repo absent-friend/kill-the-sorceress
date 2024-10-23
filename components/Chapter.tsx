74f7a2ffa581e146c849f48c8f05977a68f7423a
absent-friend <forsorenmueller@gmail.com>
2024-10-23 02:45:49 -0400

    fix stray miscolored border

--- components/Chapter.tsx
+++ components/Chapter.tsx
@@ -18,7 +18,7 @@ export default function Chapter(props: ChapterProps) {
         <h1 class="font-bold text-3xl">{props.title}</h1>
         {props.children}
       </main>
-      <nav aria-labelledby="chapter-nav-label" class="border-t border-zinc-400 grid grid-cols-[1fr_2fr_1fr] p-4">
+      <nav aria-labelledby="chapter-nav-label" class="border-t border-slate-400 dark:border-slate-700 grid grid-cols-[1fr_2fr_1fr] p-4">
         <Link href={props.previous || ''} invisible={!props.previous}>← Previous</Link>
         <span id="chapter-nav-label" class="text-center">Chapter Navigation</span>
         <Link href={props.next || ''} class="text-end" invisible={!props.next}>Next →</Link>
