c24b2a254f70d2b15a8361b1b5e68dc16c9fd9ea
absent-friend <forsorenmueller@gmail.com>
2024-11-12 15:22:09 -0500

    Chapter 3: Add videos for the Complete Force section

--- pages/(chapters)/chapter3/+Page.tsx
+++ pages/(chapters)/chapter3/+Page.tsx
@@ -1,5 +1,6 @@
 import Chapter from "../../../components/Chapter";
 import Figure from "../../../components/Figure";
+import FigureSet from "../../../components/FigureSet";
 import Section from "../../../components/Section";
 
 export default function Page() {
@@ -34,6 +35,7 @@ export default function Page() {
           can be used advantageously to compel Agent 9 to shoot at the second tank while a 
           phase-ending shot is in the air. 
         </p>
+        <Figure youtube="4S4Anf1vEno" caption="Tank I - Deboard Causes Next Tank To Drop" alt="A video demonstrating Agent 9's behavior when Spyro gets off of the first tank. Spyro jumps from the tank, and Agent 9 shoots down the next one immediately afterwards."/>
         <p>
           Normally, even if a phase-ending shot connects with the Sorceress and completes the 
           phase, the second tank will be cleared from the arena in the same way that residual 
@@ -42,6 +44,10 @@ export default function Page() {
           second tank will fall and be in the air while the arena is being cleared, which will 
           allow it to remain spawned for use during the final phase of the fight.
         </p>
+        <FigureSet>
+          <Figure youtube="nYKM1YMD1vQ" caption="Tank I - Next Tank Is Cleared After Dropping at End of Phase" alt="A video showing what happens when a tank drops at the end of the second phase with the wrong timing. Spyro shoots a shot from the first tank that deals the sixth hit of damage to the Sorceress, which causes the phase to end. In the process, a tank that Agent 9 had dropped is removed, since it touched the ground before the phase ended."/>
+          <Figure youtube="f35eXCG_Otw" caption="Tank I - Next Tank Survives After Dropping at End of Phase" alt="A video showing what happens when a tank drops at the end of the second phase with the right timing. Spyro shoots a shot from the first tank that deals the sixth hit of damage to the Sorceress, which causes the phase to end. The next tank drops after the phase transition takes place, so it isn't cleared away."/>
+        </FigureSet>
         <p>
           Simply put, before delivering phase-ending damage to the Sorceress, space the tank 
           approximately 1/2 of the arena's diameter away from the Sorceress, shoot the phase-ending 
@@ -49,6 +55,7 @@ export default function Page() {
           connects and was in the air for the correct amount of time after the tank was deboarded, 
           then the second tank that Agent 9 shoots down will remain in the arena. 
         </p>
+        <Figure youtube="Kygpt9KBvMI" caption="Tank I - Complete Force" alt="A video demonstrating the Complete Force strategy for the second phase. Spyro boards a tank and waits for the Sorceress to come to a stop somewhere. Once she does, he repositions slightly to ensure that is the correct distance away from her and fires a shot that deals the sixth hit of damage, which causes the phase to end. The next tank drops and is not cleared away."/>
         <p>
           The "Complete Force" setup can always be used as a backup for anything written below and 
           can also be used isolatedly as the main approach to the end of Phase II without 
